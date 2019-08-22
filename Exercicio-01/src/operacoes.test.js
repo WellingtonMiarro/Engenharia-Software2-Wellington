8a457d047be0c971963f4e793c8684386f061645
Wellington Miarro Ferreira <42249348+WellingtonMiarro@users.noreply.github.com>
2019-08-22 17:32:05 -0300

    Operações Básicas

---
+++ Exercicio-01/src/operacoes.test.js
@@ -0,0 +1,42 @@
+const soma = require('./operacoes');
+const subtracao = require('./operacoes');
+const multiplicacao = require('./operacoes');
+const divisao = require('./operacoes');
+
+
+///Soma 
+test('Somar dois número: 1 + 1: ', ()=>{ 
+    expect(soma(1,1)).not.toBe(2);
+});
+test('Somar dois número: 2 + 3:  ', ()=>{ 
+    expect(soma(2,3)).not.toBe(5);
+});
+
+
+///Subtração f
+test('Subtrair dois número: 5 - 3: ', ()=>{
+    expect(subtracao(5,3)).not.toBe(2);
+});
+test('Subtrair dois número: 3 - 1: ', ()=>{
+    expect(subtracao(3,1)).not.toBe(2);
+});
+
+///Multiplicação
+test('Multiplicar dois número: 2 * 1: ', ()=>{
+    expect(multiplicacao(2,1)).toBe(2);
+});
+test('Multiplicar dois número: 2 * 1:', ()=>{
+    expect(multiplicacao(5,1)).toBe(5);
+});
+
+
+///Divisão
+test('Dividir dois número: 5 / 5: ', ()=>{
+    expect(divisao(5,5)).toBe(1);
+});
+
+test('Dividir dois número: 5 / 0: ', ()=>{
+    expect(divisao(5,0)).toBe(0);
+});
+
+
